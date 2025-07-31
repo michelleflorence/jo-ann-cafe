eb601770016fc8c6560c730cbb3293f9f8a6f140
michelleflorence <michelleflorence86@gmail.com>
2025-07-31 15:52:07 +0700

    Fix ripped paper asset, add footer section (blm diisi)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import LandingPage from "./pages/LandingPage";
+import Footer from "./components/Footer/Footer";
+import rippedPaperFour from "./assets/ripped-paper-four.png";
+import styles from "./App.module.scss";
 
 const App = () => {
   return (
@@ -10,13 +13,19 @@ const App = () => {
         v7_relativeSplatPath: true,
       }}
     >
-      <div className="app-container">
+      <div className={styles["app-container"]}>
         <Navbar />
         <main className="main-content" style={{ overflowX: "hidden" }}>
           <Routes>
             <Route path="/" element={<LandingPage />} />
           </Routes>
         </main>
+        <div className={styles["footer-section"]}>
+          <div className={styles["ripped-paper-four"]}>
+            <img src={rippedPaperFour} alt="ripped paper" />
+          </div>
+          <Footer />
+        </div>
       </div>
     </BrowserRouter>
   );
